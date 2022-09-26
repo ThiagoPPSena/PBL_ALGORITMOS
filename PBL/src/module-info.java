4cd958a3f39cdd235aaeb18013be148796d463e2
ThiagoPPSena <108939221+ThiagoPPSena@users.noreply.github.com>
2022-09-26 18:00:10 -0300

    Commit realmente final

--- PBL/src/module-info.java
+++ PBL/src/module-info.java
@@ -1,6 +1,7 @@
 module a {
+	
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens application to javafx.graphics, javafxml;
+    opens application to javafx.graphics, javafx.fxml;
 }
