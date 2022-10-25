4fc6f6b0c7c0f522878649e15aa9e0606b48e6d3
Luan Barbosa <luanbscosta@hotmail.com>
2022-10-24 21:54:30 -0300

    ArbitroDAOTest e JogadorDAOTest

--- PBL/src/testes/ArbitroDAOTest.java
+++ PBL/src/testes/ArbitroDAOTest.java
@@ -2,23 +2,85 @@ package testes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+
+import application.model.Arbitro;
+import application.model.ArbitroDAO;
 
 class ArbitroDAOTest {
 
+	private static Arbitro arbitro1 = new Arbitro("Wilton");
+	private static Arbitro arbitro2 = new Arbitro("Neuza");
+	private static Arbitro arbitro3 = new Arbitro("Sandro");
+	private static Arbitro arbitro4 = new Arbitro("Anderson");
+	
 	@Test
 	void testInserirArbitro() {
-		fail("Not yet implemented");
+		//Inserindo apenas 1 objeto (Logo, o tamanho da coleção desses objetos deve ser igual a 1).
+		ArbitroDAO arbitroDAO = new ArbitroDAO();
+		arbitroDAO.InserirArbitro(arbitro1);
+		int retorno = arbitroDAO.ListaArbitro().size();
+		assertEquals(1, retorno);
+		
+		//Inserindo mais 2 objetos (Logo, o tamanho da coleção desses objetos deve ser igual a 3).
+		arbitroDAO.InserirArbitro(arbitro2);
+		arbitroDAO.InserirArbitro(arbitro3);
+		retorno = arbitroDAO.ListaArbitro().size();
+		assertEquals(3, retorno);
+		
+		//Inserindo mais 1 objeto (Logo, o tamanho da coleção desses objetos deve ser igual a 4).
+		arbitroDAO.InserirArbitro(arbitro4);
+		retorno = arbitroDAO.ListaArbitro().size();
+		assertEquals(4, retorno);
 	}
 
 	@Test
 	void testRemoverArbitro() {
-		fail("Not yet implemented");
+		//Removendo objeto que não está armazenado no DAO (Logo, deve-se retornar null).
+		ArbitroDAO arbitroDAO = new ArbitroDAO();
+		Arbitro retorno = arbitroDAO.RemoverArbitro(arbitro1.getCodArbitro());
+		assertEquals(null, retorno);
+		
+		//Removendo objeto que está armazenado no DAO (Logo, deve-se retornar o objeto).
+		arbitroDAO.InserirArbitro(arbitro1);
+		retorno = arbitroDAO.RemoverArbitro(arbitro1.getCodArbitro());
+		assertEquals(arbitro1, retorno);
+		
+		//Verificando se o objeto ainda está armazenado no DAO (Como não está, a busca deve retornar null).
+		retorno = arbitroDAO.ListaArbitro().get(arbitro1.getCodArbitro());
+		assertEquals(null, retorno);
 	}
 
 	@Test
 	void testListaArbitro() {
-		fail("Not yet implemented");
+		ArbitroDAO arbitroDAO = new ArbitroDAO();
+		
+		//Chamando a função sem nenhum objeto na lista.
+		//Fazendo com que o map que deve ser retornado seja um map vazio.
+		Map<Integer, Arbitro> retorno = arbitroDAO.ListaArbitro();
+		Map<Integer, Arbitro> esperado = new HashMap<Integer, Arbitro>();
+		assertEquals(esperado, retorno);
+		
+		//Adicionando os arbitros 1 e 4.
+		//Fazendo com que o map que deve ser retornado seja {0=arbitro1, 3=arbitro4}
+		arbitroDAO.InserirArbitro(arbitro1);
+		arbitroDAO.InserirArbitro(arbitro4);
+		retorno = arbitroDAO.ListaArbitro();
+		esperado.put(arbitro1.getCodArbitro(), arbitro1);
+		esperado.put(arbitro4.getCodArbitro(), arbitro4);
+		assertEquals(esperado, retorno);
+		
+		//Adicionando os arbitros 2 e 3.
+		//Fazendo com que o map que deve ser retornado seja {0=arbitro1, 3=arbitro4, 1=arbitro2, 2=arbitro3}
+		arbitroDAO.InserirArbitro(arbitro2);
+		arbitroDAO.InserirArbitro(arbitro3);
+		retorno = arbitroDAO.ListaArbitro();
+		esperado.put(arbitro2.getCodArbitro(), arbitro2);
+		esperado.put(arbitro3.getCodArbitro(), arbitro3);
+		assertEquals(esperado, retorno);
 	}
 
 }
--- PBL/src/testes/JogadorDAOTeste.java
+++ PBL/src/testes/JogadorDAOTeste.java
@@ -2,13 +2,112 @@ package testes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+
+import application.model.Jogador;
+import application.model.JogadorDAO;
 
 class JogadorDAOTeste {
 
+	static Jogador jogador = new Jogador("Joao", "ATACANTE");
+	static Jogador jogador1 = new Jogador("Maria", "MEIA");
+	static Jogador jogador2 = new Jogador("Enzo", "GOLEIRO");
+	static Jogador jogador3 = new Jogador("Tite", "ZAGUEIRO");
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testBuscarJogador() {
+		//Buscando um objeto que está armazenado no DAO (Logo, deve-se retornar o objeto).
+		JogadorDAO jogDAO = new JogadorDAO();
+		jogDAO.InserirJogador(jogador1);
+		Jogador retorno = jogDAO.BuscarJogador(jogador1.getCodJog());
+		assertEquals(jogador1, retorno);
+		
+		//Buscando um objeto que não está armazenado no DAO (Logo, deve-se retornar null).
+		retorno = jogDAO.BuscarJogador(jogador2.getCodJog());
+		assertEquals(null, retorno);
 	}
+	
+	@Test
+	void testInserirJogador() {
+		JogadorDAO jogDAO = new JogadorDAO();
+		int tamanhoEsperado, tamanhoObtido;
+		
+		//Sem inserir objeto Jogador na coleção, o tamanho tem que ser 0
+		tamanhoEsperado = 0;
+		tamanhoObtido = jogDAO.ListaJog().size();
+		assertEquals(tamanhoEsperado, tamanhoObtido);
+		//Inserindo 1 objeto (Logo, o tamanho da coleção desses objetos deve ser igual a 1).
+		jogDAO.InserirJogador(jogador);
+		tamanhoObtido = jogDAO.ListaJog().size();
+		tamanhoEsperado = 1;
+		assertEquals(tamanhoEsperado, tamanhoObtido);
+		//Inserindo o mesmo objeto na coleção, o tamanho permanece o mesmo: 1
+		jogDAO.InserirJogador(jogador);
+		tamanhoObtido = jogDAO.ListaJog().size();
+		assertEquals(tamanhoEsperado, tamanhoObtido);
+		//Inserindo mais 3 objeto (Logo, o tamanho da coleção desses objetos deve ser igual a 4).
+		jogDAO.InserirJogador(jogador1);
+		jogDAO.InserirJogador(jogador2);
+		jogDAO.InserirJogador(jogador3);
+		tamanhoObtido = jogDAO.ListaJog().size();
+		tamanhoEsperado = 4;
+		assertEquals(tamanhoEsperado, tamanhoObtido);
+		
+	}
+	
+	@Test
+	void testRemoverJogador() {
+		
+		Jogador jogadorEsperado, jogadorObtido;
+		JogadorDAO jogDAO = new JogadorDAO();
+		//Remover jogador sem estar na coleção, retorno é nulo
+		jogadorEsperado = null;
+		jogadorObtido = jogDAO.RemoverJogador(jogador.getCodJog());
+		assertEquals(jogadorEsperado, jogadorObtido);
+		//Remover jogador que esteja na coleção, retorna o jogador removido
+		jogDAO.InserirJogador(jogador);
+		jogadorEsperado = jogador;
+		jogadorObtido = jogDAO.RemoverJogador(jogador.getCodJog());
+		assertEquals(jogadorEsperado, jogadorObtido);
+		//Checar se após a remoção, o jogador foi realmente removido
+		jogadorEsperado = null;
+		jogadorObtido = jogDAO.ListaJog().get(jogador.getCodJog());
+		assertEquals(jogadorEsperado, jogadorObtido);
+		
+	}
+	
+	@Test
+	public void testListaJog() {
+		
+		JogadorDAO jogDAO = new JogadorDAO();
+		
+		//Chamando a função sem nenhum objeto na lista.
+		//Fazendo com que o map que deve ser retornado seja um map vazio.
+		Map<Integer, Jogador> ListaObtida = jogDAO.ListaJog();
+		Map<Integer, Jogador> ListaEsperada = new HashMap<Integer, Jogador>();
+		assertEquals(ListaEsperada, ListaObtida);
+		
+		//Adicionando os jogadors 0 e 2.
+		//Fazendo com que o map que deve ser retornado seja {0=jogador, 2=jogador2}
+		jogDAO.InserirJogador(jogador);
+		jogDAO.InserirJogador(jogador2);
+		ListaObtida = jogDAO.ListaJog();
+		ListaEsperada.put(jogador.getCodJog(), jogador);
+		ListaEsperada.put(jogador2.getCodJog(), jogador2);
+		assertEquals(ListaEsperada, ListaObtida);
+		
+		//Adicionando os jogadores 1 e 3.
+		//Fazendo com que o map que deve ser retornado seja {0=jogador1, 2=jogador2, 1=jogador1, 3=jogador3}
+		jogDAO.InserirJogador(jogador1);
+		jogDAO.InserirJogador(jogador3);
+		ListaObtida = jogDAO.ListaJog();
+		ListaEsperada.put(jogador1.getCodJog(), jogador1);
+		ListaEsperada.put(jogador3.getCodJog(), jogador3);
+		assertEquals(ListaEsperada, ListaObtida);
+	}
+
 
 }
